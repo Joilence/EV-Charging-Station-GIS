6bcd6dcbaf2d521e126519a2c0186ec745ef7fc8
Joilence <cjyk84719@gmail.com>
2021-01-11 09:43:59 +0100

    add: test process in app.component

--- frontend/code/src/app/app.component.ts
+++ frontend/code/src/app/app.component.ts
@@ -22,13 +22,21 @@ export class AppComponent implements AfterViewInit {
     // this.dataservice.getBarDistribution().subscribe((geojson: FeatureCollection) => {
     //  this.mapcomponent.addGeoJSON(geojson);
     // });
+    
+    /**
+     * Test Routing Process
+     * Konstanz [47.6779, 9.1732], Stuttgart [48.7758, 9.1829], Dresden [51.0504, 13.7373]
+     */
+    let testCor: number[][];
 
-    const testCor: number[][] = [
-      [8.681495, 49.41461],
-      [8.686507, 49.41943],
-      [8.687872, 49.420318],
+    // User Action 1: input from Konstanz to Dresden
+    testCor = [
+      [9.1732, 47.6779], // Konstanz
+      // [9.1829, 48.7758], // After 107km, Stuttgart
+      [13.7373, 51.0504], // After 507km, Dresden
     ];
 
+    // TODO: function to construct way_points
     const testWayPoints = [{
       type: 'Feature',
       properties: {
@@ -42,28 +50,22 @@ export class AppComponent implements AfterViewInit {
     }, {
       type: 'Feature',
       properties: {
-        name: 'Random Station',
-        type: 'Station'
-      },
-      geometry: {
-        type: 'Point',
-        coordinates: testCor[1]
-      }
-    }, {
-      type: 'Feature',
-      properties: {
         name: 'destination place',
         type: 'Destination'
       },
       geometry: {
         type: 'Point',
-        coordinates: testCor[2]
+        coordinates: testCor[1]
       }
     }, ];
+
+    // Get route path and display in the map
     this.dataservice.getRoute('driving-car', testCor).subscribe((geojson: GeoJSON) => {
-      //this.mapcomponent.addRoutePath(geojson);
-      //this.mapcomponent.addWayPoints(testWayPoints);
+      this.mapcomponent.addRoutePath(geojson);
+      this.mapcomponent.addWayPoints(testWayPoints);
     });
+
+    // User Action 2: Select points along the path and show isochrones with stations
 
     const testForIC = {
       locations: [[8.681495, 49.41461],
@@ -74,6 +76,13 @@ export class AppComponent implements AfterViewInit {
     this.dataservice.getIsochrones(testForIC.locations, 'distance', testForIC.range).subscribe((features: FeatureCollection) => {
       this.mapcomponent.addIsochrones(features);
     });
+
+    // User Action 3: Select stations and re-route
+    testCor = [
+      [9.1732, 47.6779], // Konstanz
+      [9.1829, 48.7758], // After 107km, Stuttgart
+      [13.7373, 51.0504], // After 507km, Dresden
+    ];
 
   }
 }
