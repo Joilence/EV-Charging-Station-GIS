fcb3444952911dc1ab6e0c0fb5781aa4540593a4
Daniel Muffler <daniel.muffler@uni-konstanz.de>
2021-01-16 12:57:00 +0100

    Fix

--- frontend/code/src/app/app.component.ts
+++ frontend/code/src/app/app.component.ts
@@ -1,18 +1,18 @@
 /// <reference types='leaflet-sidebar-v2' />
-import { AfterContentInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Feature, FeatureCollection } from 'geojson';
-import { MapComponent } from './map/map.component';
-import { DataService } from './services/data.service';
-import { RoutingService } from './services/routing.service'
-import { Map, SidebarOptions } from 'leaflet';
-import { SpinnerOverlayService } from './services/spinner-overlay.service';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Feature, FeatureCollection} from 'geojson';
+import {MapComponent} from './map/map.component';
+import {DataService} from './services/data.service';
+import {RoutingService} from './services/routing.service';
+import {Map, SidebarOptions} from 'leaflet';
+import {SpinnerOverlayService} from './services/spinner-overlay.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterContentInit {
+export class AppComponent {
 
   public map!: Map;
 
@@ -23,27 +23,31 @@ export class AppComponent implements AfterContentInit {
     container: 'sidebar',
   };
 
-  @ViewChild(MapComponent) mapcomponent!: MapComponent;
+  @ViewChild(MapComponent) mapComponent!: MapComponent;
 
-  @ViewChild('inputStart', { static: true })
+  @ViewChild('inputStart', {static: true})
   inputStart!: ElementRef;
-  @ViewChild('inputTarget', { static: true })
+  @ViewChild('inputTarget', {static: true})
   inputTarget!: ElementRef;
-  @ViewChild('inputRange', { static: true })
+  @ViewChild('inputRange', {static: true})
   inputRange!: ElementRef;
 
   /*
    * Services or other dependencies are often imported via dependency injection.
    * See https://angular.io/guide/dependency-injection for more details.
    */
-  constructor(private dataservice: DataService, private routingservice: RoutingService, private spinnerService: SpinnerOverlayService) { }
+  constructor(private dataservice: DataService, private routingservice: RoutingService, private spinnerService: SpinnerOverlayService) {
+  }
 
   receiveMap(map: Map): void {
     // This will throw an ExpressionChangedAfterItHasBeenCheckedError error in dev mode. That's okay and not problematic.
     this.map = map;
+    setTimeout(() => {
+      this.initTestProcess();
+    }, 2000);
   }
 
-  ngAfterContentInit(): void {
+  initTestProcess(): void {
     // this.dataservice.getBarDistribution().subscribe((geojson: FeatureCollection) => {
     //  this.mapcomponent.addGeoJSON(geojson);
     // });
@@ -56,35 +60,35 @@ export class AppComponent implements AfterContentInit {
      * Konstanz [47.6779, 9.1732], Stuttgart [48.7758, 9.1829], Dresden [51.0504, 13.7373]
      */
 
-    // User Action 1: input from Konstanz to Dresden
+      // User Action 1: input from Konstanz to Dresden
     const initLocations: FeatureCollection = {
-      type: 'FeatureCollection',
-      features: [{
-        type: 'Feature',
-        properties: {
-          name: 'Konstanz',
-          type: 'Departure'
-        },
-        geometry: {
-          type: 'Point',
-          coordinates: [9.1732, 47.6779], // Konstanz
-        }
-      }, {
-        type: 'Feature',
-        properties: {
-          name: 'Dresden',
-          type: 'Destination'
-        },
-        geometry: {
-          type: 'Point',
-          coordinates: [13.7373, 51.0504], // Dresden
-        }
-      },]
-    }
+        type: 'FeatureCollection',
+        features: [{
+          type: 'Feature',
+          properties: {
+            name: 'Konstanz',
+            type: 'Departure'
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [9.1732, 47.6779], // Konstanz
+          }
+        }, {
+          type: 'Feature',
+          properties: {
+            name: 'Dresden',
+            type: 'Destination'
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [13.7373, 51.0504], // Dresden
+          }
+        }, ]
+      };
 
     this.routingservice.initDepDest(initLocations);
 
-    this.mapcomponent.addRoutePath(this.routingservice.getCurrentRoute());
+    this.mapComponent.addRoutePath(this.routingservice.getCurrentRoute());
 
     // User Action 2: Select points along the path and show isochrones with stations
     setTimeout(() => {
@@ -93,28 +97,28 @@ export class AppComponent implements AfterContentInit {
         range: [10000]
       };
       this.dataservice.getIsochrones(selectedPoint.location, 'distance', selectedPoint.range).subscribe((isochrones: FeatureCollection) => {
-        this.mapcomponent.addIsochrones(isochrones);
+        this.mapComponent.addIsochrones(isochrones);
       });
 
       this.dataservice.getStations(selectedPoint.location, selectedPoint.range).subscribe((stations: FeatureCollection) => {
-        this.mapcomponent.addStations(stations);
-      })
-    }, 3000)
+        this.mapComponent.addStations(stations);
+      });
+    }, 3000);
 
     // User Action 3: Select stations and re-route
     setTimeout(() => {
       const selectedStation: Feature = {
-        'type': 'Feature',
-        'geometry': {
-          'type': 'Point',
-          'coordinates': [9.178289, 48.774372],
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [9.178289, 48.774372],
         },
-        'properties': {
-          'address': 'Eichstraße 7',
+        properties: {
+          address: 'Eichstraße 7',
         }
-      }
+      };
       this.routingservice.addNewStation(selectedStation);
-      this.mapcomponent.addRoutePath(this.routingservice.getCurrentRoute());
+      this.mapComponent.addRoutePath(this.routingservice.getCurrentRoute());
     }, 5000);
   }
 
