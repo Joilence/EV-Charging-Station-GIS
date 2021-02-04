7d17a70ff1495803db5465644f1191b656744c91
Joilence <cjyk84719@gmail.com>
2021-02-04 23:21:41 +0100

    chore

--- frontend/code/src/app/map/map.component.ts
+++ frontend/code/src/app/map/map.component.ts
@@ -498,7 +498,8 @@ export class MapComponent {
     // console.log('looking for stations by id:', this.stationsFeatureCollectionCache);
     for (const station of (this.stationsFeatureCollectionCache as FeatureCollection<Point>).features) {
       // console.log('check:', station.id as number);
-      if (station.id as number === stationID) {
+      // tslint:disable-next-line:triple-equals
+      if (station.id as number == stationID) {
         // console.log(station);
         // console.log(station as Feature);
         return station;
